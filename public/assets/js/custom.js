8dfe87177e2761ee815fd6c30a6f28137573b5de
Andres Perez Melo <61465925+andresperezmelo@users.noreply.github.com>
2025-02-12 10:35:06 -0500

    Update page

---
+++ public/assets/js/custom.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1, // Muestra una diapositiva por vista
+    spaceBetween: 30, // Espacio entre diapositivas en píxeles
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
